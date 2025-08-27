3c3e0c1eefb9425f39b4a38785fcd868440b0c38
susanna3601 <susanna060503@gmail.com>
2025-08-27 17:05:48 +0200

    Add files via upload

---
+++ OppgaveO1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class OppgaveO1 {
+    public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+
+        System.out.println("Hva er bruttoinntekten din? ");
+        String BrukerInput = input.nextLine();
+
+        BrukerInput = BrukerInput.replace(" ","");
+        double Bruttoinntekt = Double.parseDouble(BrukerInput);
+
+        double Trinnskatt = 0.0;
+
+        if (Bruttoinntekt <= 217400) {
+            System.out.println("Ingen trinnskatt");
+        } else {
+            if (Bruttoinntekt >=217401 && Bruttoinntekt <= 306050) {
+            Trinnskatt = Bruttoinntekt * 0.0017;
+        } else if (Bruttoinntekt >=306051 && Bruttoinntekt <= 697150) {
+            Trinnskatt = Bruttoinntekt * 0.0040;
+        } else if (Bruttoinntekt >=697151 && Bruttoinntekt <=942400) {
+            Trinnskatt = Bruttoinntekt * 0.0137;
+        } else if (Bruttoinntekt >=942401 && Bruttoinntekt <= 1410750) {
+            Trinnskatt = Bruttoinntekt * 0.0167;
+        } else {
+            Trinnskatt = Bruttoinntekt * 0.0177;
+        }
+            System.out.printf("Din trinnskatt er: %.2f kr", Trinnskatt);
+        }
+
+        input.close();
+    }
+}
+
