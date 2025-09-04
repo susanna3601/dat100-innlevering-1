8ff985595773faa7150ad7ba9e99e2f2a2d213ec
susanna3601 <susanna060503@gmail.com>
2025-09-04 10:24:03 +0200

    Update OppgaveO1.java

--- OppgaveO1.java
+++ OppgaveO1.java
@@ -16,15 +16,15 @@ public class OppgaveO1 {
             System.out.println("Ingen trinnskatt");
         } else {
             if (Bruttoinntekt >=217401 && Bruttoinntekt <= 306050) {
-            Trinnskatt = Bruttoinntekt * 0.0017;
+            Trinnskatt = Bruttoinntekt * 0.017;
         } else if (Bruttoinntekt >=306051 && Bruttoinntekt <= 697150) {
-            Trinnskatt = Bruttoinntekt * 0.0040;
+            Trinnskatt = Bruttoinntekt * 0.040;
         } else if (Bruttoinntekt >=697151 && Bruttoinntekt <=942400) {
-            Trinnskatt = Bruttoinntekt * 0.0137;
+            Trinnskatt = Bruttoinntekt * 0.137;
         } else if (Bruttoinntekt >=942401 && Bruttoinntekt <= 1410750) {
-            Trinnskatt = Bruttoinntekt * 0.0167;
+            Trinnskatt = Bruttoinntekt * 0.167;
         } else {
-            Trinnskatt = Bruttoinntekt * 0.0177;
+            Trinnskatt = Bruttoinntekt * 0.177;
         }
             System.out.printf("Din trinnskatt er: %.2f kr", Trinnskatt);
         }
